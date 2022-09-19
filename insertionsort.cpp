167efa273b981be70a49a8d83a8372e18c58c306
mahesh3536 <maheshjivarani3536@gmail.com>
2022-09-19 13:31:42 +0530

    insertion sort

---
+++ insertionsort.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void insertionsort(int arr[],int n){
+    for(int i = 1;i<n;i++){
+        int temp = arr[i];
+        int j = i-1;
+        for( ; j>=0 ;j--){
+            if(arr[j]>temp){
+                arr[j+1] = arr[j];
+            }
+            else {
+                break;
+            }
+        }
+        arr[j+1]=temp;
+    }
+}
+int main(){
+    int arr[7]={4,2,5,1,3,8,7};
+    insertionsort(arr,7);
+    for(int i = 0;i<7;i++) cout << arr[i] << " ";
+}
