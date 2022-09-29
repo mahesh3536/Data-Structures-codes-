be60274d3395057b821e5af0fe9451bc6fe29e4a
mahesh3536 <maheshjivarani3536@gmail.com>
2022-09-29 19:20:10 +0530

    format document

--- circular_queue_using_array.cpp
+++ circular_queue_using_array.cpp
@@ -1,142 +1,178 @@
-//circular queue implementation 
-#include<iostream>
+// circular queue implementation
+#include <iostream>
 using namespace std;
-class Queue{
-    public:
-    int* arr;
+class Queue
+{
+public:
+    int *arr;
     int f;
     int r;
     int size;
-    Queue(int d){
-       this->size=d;
-       arr = new int[d];
-       this->f=-1;
-       this->r=-1;
+    Queue(int d)
+    {
+        this->size = d;
+        arr = new int[d];
+        this->f = -1;
+        this->r = -1;
     }
-    bool isEmpty(){
-        if(f==-1 && r==-1) return true;
-        else return false;
+    bool isEmpty()
+    {
+        if (f == -1 && r == -1)
+            return true;
+        else
+            return false;
     }
-    bool isFull(){
-        if((r+1)%size == f) return true;
-        else return false;
+    bool isFull()
+    {
+        if ((r + 1) % size == f)
+            return true;
+        else
+            return false;
     }
-    int front(){
-        if(f==-1 && r == -1){
+    int front()
+    {
+        if (f == -1 && r == -1)
+        {
             cout << "Queue is empty" << endl;
             return 0;
         }
-        else{
+        else
+        {
             return arr[f];
         }
     }
-    int rear(){
-        if(f==-1 && r==-1){
+    int rear()
+    {
+        if (f == -1 && r == -1)
+        {
             cout << "Queue is empty" << endl;
             return 0;
         }
-        else return arr[r];
+        else
+            return arr[r];
     }
-    void enQueue(int val){
-        if((r+1)%size == f){
+    void enQueue(int val)
+    {
+        if ((r + 1) % size == f)
+        {
             cout << "Queue is Full" << endl;
             return;
         }
-        else if(f==-1 && r==-1){
+        else if (f == -1 && r == -1)
+        {
             f++;
             r++;
-            arr[r]=val;
+            arr[r] = val;
         }
-        else{
-            r = (r+1)%size;
-            arr[r]=val;
+        else
+        {
+            r = (r + 1) % size;
+            arr[r] = val;
         }
     }
-    int deQueue(){
-        if(f==-1 && r==-1){
+    int deQueue()
+    {
+        if (f == -1 && r == -1)
+        {
             cout << "Queue is empty" << endl;
             return 0;
         }
-        else if(f==r){
+        else if (f == r)
+        {
             int x = arr[f];
-            f=-1;
-            r=-1;
+            f = -1;
+            r = -1;
             return x;
         }
-        else{
+        else
+        {
             int x = arr[f];
-            f = (f+1)%size;
+            f = (f + 1) % size;
             return x;
         }
     }
 };
-int main(){
-   int size;
-   cout << "Enter size of Queue : " ;
-   cin >> size;
-   Queue Q(size);
-   int val;
-   do{
-      cout << "1.Enter 1 for isEmpty" << endl;
-      cout << "2.Enter 2 for isFull" << endl;
-      cout << "3.Enter 3 for enQueue"<< endl;
-      cout << "4.Enter 4 for deQueue"<<endl;
-      cout<< "5.Enter 5 for front element"<<endl;
-      cout<< "6.Enter 6 for rear element"<<endl;
-      cout<< "7.Enter 7 for exit"<< endl;
-      cin >> val;
-      switch(val){
+int main()
+{
+    int size;
+    cout << "Enter size of Queue : ";
+    cin >> size;
+    Queue Q(size);
+    int val;
+    do
+    {
+        cout << "1.Enter 1 for isEmpty" << endl;
+        cout << "2.Enter 2 for isFull" << endl;
+        cout << "3.Enter 3 for enQueue" << endl;
+        cout << "4.Enter 4 for deQueue" << endl;
+        cout << "5.Enter 5 for front element" << endl;
+        cout << "6.Enter 6 for rear element" << endl;
+        cout << "7.Enter 7 for exit" << endl;
+        cin >> val;
+        switch (val)
+        {
         case 1:
         {
-            if(Q.isEmpty()) cout << "Queue is empty" << endl;
-            else cout << "Queue is not empty" << endl;
+            if (Q.isEmpty())
+                cout << "Queue is empty" << endl;
+            else
+                cout << "Queue is not empty" << endl;
         }
         break;
         case 2:
         {
-            if(Q.isFull()) cout << "Queue is Full" << endl;
-            else cout << "Queue is not Full" << endl;
-
+            if (Q.isFull())
+                cout << "Queue is Full" << endl;
+            else
+                cout << "Queue is not Full" << endl;
         }
         break;
         case 3:
-        {   if(Q.isFull()){
-               cout << "Queue is Full" << endl;
+        {
+            if (Q.isFull())
+            {
+                cout << "Queue is Full" << endl;
             }
-            else{
-            int data;
-            cout << "Enter data for enQueue : " ;
-            cin >> data;
-            Q.enQueue(data);
-            cout << "enQueue done" << endl;
+            else
+            {
+                int data;
+                cout << "Enter data for enQueue : ";
+                cin >> data;
+                Q.enQueue(data);
+                cout << "enQueue done" << endl;
             }
         }
         break;
         case 4:
         {
-            if(Q.isEmpty()) cout << "Queue is Empty" << endl;
-            else {
+            if (Q.isEmpty())
+                cout << "Queue is Empty" << endl;
+            else
+            {
                 cout << "DeQueue element of Queue is : ";
                 cout << Q.deQueue() << endl;
-                
             }
         }
         break;
         case 5:
         {
-            if(Q.isEmpty()) cout << "Queue is Empty" << endl;
-            else {
+            if (Q.isEmpty())
+                cout << "Queue is Empty" << endl;
+            else
+            {
                 cout << "Front element of Queue is : " << Q.front() << endl;
             }
         }
         break;
         case 6:
         {
-           if(Q.isEmpty()) cout << "Queue is Empty" << endl;
-           else {
-               cout << "Rear element of Queue is : " << Q.rear() << endl;
-           }
+            if (Q.isEmpty())
+                cout << "Queue is Empty" << endl;
+            else
+            {
+                cout << "Rear element of Queue is : " << Q.rear() << endl;
+            }
         }
-      }
-   }while(val!=7);
+        }
+    } while (val != 7);
 }
