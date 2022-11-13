117c82a3b34c5a8a239f4089945f3177e7fb4a4a
mahesh3536 <maheshjivarani3536@gmail.com>
2022-11-13 14:12:20 +0530

    disjoint set and weighted graph

--- weighted_graph.cpp
+++ weighted_graph.cpp
@@ -1,5 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
+class DisjointSet{
+    vector<int> rank,parent,size;
+    public: 
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i = 0;i<=n;i++){
+            parent[i]=i;
+            size[i]= 1;
+        }
+    }
+    int findUPar(int node){
+        if(node == parent[node]) {
+            return node;
+        }
+        return parent[node]=findUPar(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_v] < rank[ulp_u]){
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+    
+};
 class graph{
    public:
    int nodes;
@@ -11,6 +59,7 @@ class graph{
    void addEdge(int , int ,int ,bool);
    void shortest_path(int);
    void prim_algorithm(int);
+   void Kruskal_algorithm();
 };
 int main(){
    graph g (7);
@@ -26,6 +75,8 @@ int main(){
 
    cout << endl;
    g.prim_algorithm(1);
+    cout << endl;
+   g.Kruskal_algorithm();
 }
 
 void graph :: print() {
@@ -80,6 +131,7 @@ void graph :: prim_algorithm(int S){
     vector<int> vis(this->nodes ,0);
     unordered_map<int,list<vector<int>>> adj1;
     pq.push({0,S});
+    int minWt = 0;
     while(!pq.empty()){
         auto it = pq.top();
         pq.pop();
@@ -87,6 +139,7 @@ void graph :: prim_algorithm(int S){
         int wt = it.first;
         if(vis[node]==1) continue;
         int mini = INT_MAX;
+        minWt+=wt;
         for(auto it : adj[node]){
             if(it[1]==wt){
                mini = min(mini,it[0]);
@@ -104,7 +157,7 @@ void graph :: prim_algorithm(int S){
             }
         }
     }
-
+   cout << "Weight of minimum spannig tree is : " << minWt << endl;
    for (auto i : adj1)
         {
             cout << i.first<< "->";
@@ -113,5 +166,42 @@ void graph :: prim_algorithm(int S){
             }
             cout << endl;
         }
-
+   cout << endl;
 } 
+void graph :: Kruskal_algorithm(){
+    vector<pair<int,pair<int,int>>> edges;
+    unordered_map<int,list<vector<int>>> adj1;
+    for(int i = 1 ; i<=this->nodes;i++){
+        for(auto it : adj[i]){
+            int adjNode = it[0];
+            int wt = it[1];
+            int node = i;
+            edges.push_back({wt,{node,adjNode}});
+        }
+    }
+    sort(edges.begin(),edges.end());
+    DisjointSet ds(this->nodes);
+    int minWt = 0;
+    for(auto it : edges){
+        int weight = it.first ;
+        int u = it.second.first ;
+        int v = it.second.second;
+        if(ds.findUPar(u) != ds.findUPar(v)){
+             adj1[u].push_back({v,weight});
+             adj1[v].push_back({u,weight});
+             minWt+=weight;
+             ds.unionBySize(u,v);
+        }
+    }
+    cout << "Weight of minimum spannig tree is : " << minWt << endl;
+    //printing minimum spanning tree 
+    for (auto i : adj1)
+        {
+            cout << i.first<< "->";
+            for(auto j : i.second){
+                cout << "{" << j[0] << "," << j[1] << "}" << " ";
+            }
+            cout << endl;
+        }
+   cout << endl;
+}
